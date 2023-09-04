13c2e1af50fa335d2afb8a2cd5ed34e6ec6f3ace
elishamutang <elishamutang@outlook.com>
2023-09-04 21:24:41 +0800

    Added loop to create 256 square units

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const bigCon = document.getElementById("big-con");
 
-for (let i=0; i < 16; i++) {
+for (let i=0; i < 256; i++) {
     const smallDiv = document.createElement("div");
     smallDiv.className = "small-con";
     bigCon.appendChild(smallDiv);
