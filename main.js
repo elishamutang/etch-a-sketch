8555e6b226579cfef9eba33fb74d680892ed96cd
elishamutang <elishamutang@outlook.com>
2023-09-04 22:46:41 +0800

    Added button for each small box in grid.

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const bigCon = document.getElementById("big-con");
 
-for (let i=0; i < 256; i++) {
-    const smallDiv = document.createElement("div");
+
+for (let i=1; i < 257; i++) {
+    smallDiv = document.createElement("div");
     smallDiv.className = "small-con";
     bigCon.appendChild(smallDiv);
+}
+
+const smallBox = document.querySelectorAll(".small-con");
+
+for (let box of smallBox) {
+    box.addEventListener("click", function() {
+        box.style.backgroundColor = "black";
+    })
 }
